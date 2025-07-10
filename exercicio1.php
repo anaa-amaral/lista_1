cad94a07d245b5f21b701192d06b9f070171030b
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-07-10 07:45:34 -0300

    Exercicio 1 - finalizado

---
+++ exercicio1.php
@@ -0,0 +1,33 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificar par ou ímpar</title>
+</head>
+<body>
+
+   <form method="POST" action="">
+        <label for="numero">Verifica se é um número primo:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_numero">Verificar</button>
+   </form>
+
+   <?php
+
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+   $numero = $_POST['numero'];
+   
+   if ($numero % 2 === 0) {
+        echo "O número $numero é par ";
+    } else {
+        echo "O número $numero é ímpar ";
+    }
+
+}
+
+
+   ?>
+ 
+</body>
+</html>
