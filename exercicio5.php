edd73dd175df7041ca38da34d91ac5b5ea61ef16
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-07-10 09:25:48 -0300

    Exercicio 5 - finalizado

--- exercicio5.php
+++ exercicio5.php
@@ -9,25 +9,37 @@
    <form method="POST" action="">
         <label for="numeros"> Verificar se dois números formam um número amigo:</label>
         <input type="number" id="numero1" name="numero1" required>
-        <button type="submit" name="verificar_amigo">Verificar</button>
         <input type="number" id="numero2" name="numero2" required>
         <button type="submit" name="verificar_amigo">Verificar</button>
    </form>
 
    <?php
 
-   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    function somarDivisores($n){
 
-   $numero = $_POST['numero'];
-   
-    if($numero > 0){
-        echo 'O número é positivo!';
-    } else if ($numero < 0){
-        echo 'O número é negativo';
-    } else{
-        echo 'O número é zero';
+        $soma = 0;
+        for($i = 1; $i < $n; $i++){
+
+            if($n % $i == 0){
+                $soma += $i;
+
+            };
+        };
+        return $soma;
     };
-}
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST' && isset ( $_POST [ 'verificar_amigo'])){
+
+            $a = $_POST['numero1'];
+            $b = $_POST['numero2'];
+
+
+            if(somarDivisores($a) == $b && somarDivisores($b) == $a){
+                echo " $a e $b são amigos";
+            }else{
+                echo " $a e $b não são amigos";
+            };
+        };
 
 
    ?>
